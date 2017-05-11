249a23f2cf2fc3e97dd3d4c8eac0473ba6324836
菅健一郎 <complete1317@hotmail.co.jp>
2017-05-12 02:32:42 +0900

    test

--- OseroTest/OseroTest.py
+++ OseroTest/OseroTest.py
@@ -1,53 +1,46 @@
 import os
 
-
-
-board = [[0 for col in range(9)] for row in range(9)]
-PlayerAttribute=1
-for a in range(0,9):
-    for b in range(0,9):
-        board[a][b]=0
-
-#盤面初期設定
-def Initialize():
-    board[3][3]=1
-    board[4][4]=1
-    board[4][3]=2
-    board[3][4]=2
-
 #コンソール上に基盤の表示
 def SeeBoard():
     os.system('cls')
-    for a in range(0,9):
-        for b in range(0,9):
+    print("012345678")
+    for a in range(0,8):
+        print(a+1,end='')
+        for b in range(0,8):
             print(board[b][a],end='')
-        print('\n')
+        print()
 
 #石の設置
 def move():
-    print("You are "+PlayerAttribute)
+    global PlayerAttribute
+    print("You are ",end='')
+    print(PlayerAttribute)
     print("Where do you want to move?")
-    x=input()
-    y=input()
-    board[b-1][a-1]=PlayerAttribute
-    if(PlayerAttribute=='1'):
+    x,y= map(int, input().split())
+    board[x-1][y-1]=PlayerAttribute
+    if(PlayerAttribute==1):
         PlayerAttribute=2
     else:
         PlayerAttribute=1
 
 
-
-Initialize()
-SeeBoard()
-move()
-SeeBoard()
+#盤面初期設定
+board = [[0 for col in range(8)] for row in range(8)]
+PlayerAttribute=1
+for a in range(0,8):
+    for b in range(0,8):
+        board[a][b]=0
+board[3][3]=1
+board[4][4]=1
+board[4][3]=2
+board[3][4]=2
 
 while(True):
-    print("Player:",end='')
-    p=input()
-    if(p=='1'):
-        print("you are 1")
-    if(p=='2'):
-        print("you are 2")
-    else:
-        print("error")
+    SeeBoard()
+    move()
+    SeeBoard()
+    move()
+'''
+print("Player:",end='')
+print(PlayerAttribute)
+'''
